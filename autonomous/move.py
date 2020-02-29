85f362c668464891009bcbd25fc1be7b9bb42c5a
Boran Seckin <me@boranseckin.com>
2020-02-29 17:23:48 -0500

    Fix states

--- autonomous/move.py
+++ autonomous/move.py
@@ -6,14 +6,14 @@ from common import vision
 from magicbot.state_machine import state, timed_state, AutonomousStateMachine
 
 class Middle(BaseAuto):
-    MODE_NAME = "Middle"
+    MODE_NAME = "Default"
     DEFAULT = True
 
     drive: swervedrive.SwerveDrive
     shooter: shooter.Shooter
     vision = vision.Vision()
 
-    @timed_state(duration=10, next_state="shoot", first=True)
+    @state(first=True)
     def vision_align(self):
         drive = self.vision.verticalAdjust()
         rotate = self.vision.horizontalAdjust()
@@ -26,6 +26,7 @@ class Middle(BaseAuto):
                 self.drive.set_raw_rcw(rotate * 0.35)
             else:
                 self.drive.set_raw_rcw(0)
+                self.next_state('shoot')
 
         self.drive.update_smartdash()
 
@@ -34,21 +35,11 @@ class Middle(BaseAuto):
         self.shooter.shoot()
 
 class Left(BaseAuto):
-    MODE_NAME = "Left"
+    MODE_NAME = "Only Move"
     DEFAULT = False
 
     drive: swervedrive.SwerveDrive
 
-    @timed_state(duration=5, next_state="finish", first=True)
-    def drive_left(self):
-        self.drive.set_raw_strafe(0.25)
-
-class Right(BaseAuto):
-    MODE_NAME = "Right"
-    DEFAULT = False
-
-    drive: swervedrive.SwerveDrive
-
-    @timed_state(duration=5, next_state="right", first=True)
-    def drive_left(self):
-        self.drive.set_raw_strafe(-0.25)
+    @timed_state(duration=3, next_state="finish", first=True)
+    def drive(self):
+        self.drive.set_raw_fwd(0.35)
