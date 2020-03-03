c69dce193f5f09c4d9a2abed5e097104346195b6
Boran Seckin <me@boranseckin.com>
2020-03-02 19:06:23 -0500

    Change getValues for vision

--- common/vision.py
+++ common/vision.py
@@ -3,29 +3,39 @@ import math
 
 from components import swervedrive
 from networktables import NetworkTables
+from networktables.util import ntproperty
 
 class Vision():
+    # Main networktable
     table = NetworkTables.getTable('limelight')
+    # Horizontal offset from croshair to target in degrees
+    tx = ntproperty('/limelight/tx', 0)
+    # Vertical offset from croshair to target in degress
+    ty = ntproperty('/limelight/ty', 0)
+    # Whether the limelight has any valid targets
+    tv = ntproperty('/limelight/tv', 0)
 
     KpHorizontal = -0.6 # Proportional control constant for adjustment in horizontal
     KpVertical = -0.3 # Proportional control constant for adjustment in vertical
 
     cam_height = 38.5 # Camera's height from the ground in inches
-    cam_angle = 110 # Camera's angle from the horizontal in degrees
+    cam_angle = 70 # Camera's angle from the horizontal in degrees
 
     target_height = 98.25 # Target's mid-point's height from the ground in inches
     target_distance = 120 # Desired distance from the wall to shoot
-    
-    tv = 0 # Whether the limelight has any valid targets
-    tx = 0 # Horizontal offset from croshair to target in degrees
-    ty = 0 # Vertical offset from croshair to target in degress
 
     debug = False
 
     def getValues(self):
-        self.tv = self.table.getNumber('tv', 0)
-        self.tx = self.table.getNumber('tx', 0)
-        self.ty = self.table.getNumber('ty', 0)
+        '''
+        Get values from the Limelight networktable.
+        '''
+        values = dict()
+        values['tx'] = self.table.getNumber('tx', 0)
+        values['ty'] = self.table.getNumber('ty', 0)
+        values['tv'] = self.table.getNumber('tv', 0)
+
+        return values
 
     @staticmethod
     def degree_to_rad(degree):
@@ -38,8 +48,6 @@ class Vision():
         '''
         # https://docs.limelightvision.io/en/latest/cs_estimating_distance.html
         
-        self.getValues()
-
         a1 = self.degree_to_rad(self.ty)
         a2 = self.degree_to_rad(self.cam_angle)
 
@@ -48,11 +56,9 @@ class Vision():
 
     def verticalAdjust(self):
         '''
-        Get the required vertical adjustment (drive) to align.
-        :return: [-1, 1]
+        Return the required vertical adjustment (drive) to align.
+        :return: Clamped error [-1, 1]
         '''
-        self.getValues()
-
         error = self.ty
         adjust = max(min(error, 1), -1)
         return adjust
@@ -60,16 +66,14 @@ class Vision():
     def horizontalAdjust(self):
         '''
         Get the required horizontal adjustment (rotate) to align.
-        :return: [-1, 1]
+        :return: Clamped error [-1, 1]
         '''
-        self.getValues()
-
         error = self.tx
         adjust = max(min(error, 1), -1)
         return adjust
 
     def updateTable(self):
         if self.debug:
-            self.table.putNumber('Rotate', self.horizontalAdjust())
             self.table.putNumber('Drive', self.verticalAdjust())
+            self.table.putNumber('Rotate', self.horizontalAdjust())
             self.table.putNumber('Distance', self.getDistance())
